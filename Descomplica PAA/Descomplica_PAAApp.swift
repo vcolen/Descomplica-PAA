c847e2504a057a9cd73780295fb5feffb74f8925
VIctor <colen.victor@icloud.com>
2024-07-07 09:13:45 -0300

    Initial Commit

---
+++ Descomplica PAA/Descomplica_PAAApp.swift
@@ -0,0 +1,17 @@
+//
+//  Descomplica_PAAApp.swift
+//  Descomplica PAA
+//
+//  Created by Victor Colen on 07/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Descomplica_PAAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
